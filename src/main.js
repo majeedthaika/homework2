19adc52b5ecf5b6e51e619872da9bc56056921e0
Majeed Thaika <majeed.thaika@hotmail.com>
2017-03-18 17:39:38 +0700

    added own heroku server

--- src/main.js
+++ src/main.js
@@ -13,7 +13,8 @@ Vue.use(Element)
 import Axios from 'axios'
 Vue.$http = Axios
 
-Axios.defaults.baseURL = 'https://iccs340-vue-api-auth.herokuapp.com/'
+// Axios.defaults.baseURL = 'https://iccs340-vue-api-auth.herokuapp.com/'
+Axios.defaults.baseURL = 'https://mj-homework2.herokuapp.com/'
 Axios.defaults.headers.common.Accept = 'application/json'
 Axios.defaults.withCredentials = true
 
