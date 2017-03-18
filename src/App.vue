d360769cbead94806a60e72d635be31ed0fdb306
Majeed Thaika <majeed.thaika@hotmail.com>
2017-03-18 16:50:47 +0700

    added element

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="sign-out" style="float: right;">
+      <el-button type="primary" @click.native="logout">Logout</el-button>
+    </div>
+    <br><br><br>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import UsersApi from './api/users.js'
+import router from './router'
+
 export default {
-  name: 'app'
+  name: 'sign-out',
+  methods: {
+    logout () {
+      console.log('methods.logout')
+      UsersApi.logout(function (_response) {
+        router.push({ name: 'Users.sign_in' })
+      })
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,30 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+
+import Element from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+
+Vue.use(Element)
+
+import Axios from 'axios'
+Vue.$http = Axios
+
+Axios.defaults.baseURL = 'https://iccs340-vue-api-auth.herokuapp.com/'
+Axios.defaults.headers.common.Accept = 'application/json'
+Axios.defaults.withCredentials = true
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  data: {
+    isLoggedIn: false
+  },
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
